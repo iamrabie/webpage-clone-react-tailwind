e5a4a9751ee09a5b36bcd482f7f5f161920474d3
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2024-10-20 21:40:04 +0500

    custom tailwind css classes added in index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import Footer from "./components/footer/footer.component";
+import LatestBlogs from "./components/latestBlogs/latestBlogs.component";
+import Pagination from "./components/pagination/pagination.component";
+import Navbar from "./components/navbar/navbar.component";
+import Blogs from "./components/blogs/blogs.component";
+
 function App() {
   return (
-    <div className="App">
-       <p className="text-4xl">gggg</p>
+    <>
+    <div className="pt-[140px] pb-[90px] ">
+       {/* MAIN CONTAINER */}
+       <div className="container">
+         <Navbar />
+         <Blogs />
+        {/*  latest blogs container */}
+         <LatestBlogs />
+        {/* pagination */}
+         <Pagination />
+       </div>
+       <hr></hr>
+       <Footer />
     </div>
+   
+    </>
   );
 }
 
